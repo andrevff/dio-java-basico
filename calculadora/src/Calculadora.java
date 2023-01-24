d491b65dfdc39e6ab470db729ba5504c9fbda58e
Andre Ferreira <andrevcandido61@gmail.com>
2023-01-23 22:22:14 -0300

    exemplo de java-doc

---
+++ calculadora/src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operçãoes matemáticas entre números inteiros.
+ * <p>
+ * <b>Note:<b/> Leia atentamente a documentação dessas classes para desfrutar dos recursos oferecidos pelo autor.
+ * 
+ * @author André Ferreira
+ * @version 1.0
+ * @since 23/01/2023
+ */
+
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é p primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return o resultado deste método é a soma dos dois números
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
